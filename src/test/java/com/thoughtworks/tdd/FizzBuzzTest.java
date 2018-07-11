d8daf87d5f9824444664282058a798ed1f2215db
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-11 11:11:44 +0800

    [jessie & tracy]completed when input is 1

---
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_call_fizzbuzz_when_input_is_1(){
+        //given
+        int number=1;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String result =fizzBuzz.fizzBuzz(number);
+        //then
+        assertThat(result,is("1"));
+    }
+}
